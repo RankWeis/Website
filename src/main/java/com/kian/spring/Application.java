244dd055065ddbb40429a976d7906276e0042262
Kian <kian.samii@gmail.com>
2014-10-05 04:08:48 -0700

    Adding controller for Roman Numeral.

--- src/main/java/com/kian/spring/Application.java
+++ src/main/java/com/kian/spring/Application.java
@@ -1,6 +1,8 @@
 package com.kian.spring;
 
 import com.kian.sandbox.romanNumerals.RomanNumeralConverter;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -16,6 +18,7 @@ import java.util.Arrays;
 @EnableAutoConfiguration
 @ComponentScan({"com.kian.spring"})
 public class Application {
+	private static final Logger log = LogManager.getLogger(Application.class);
 
     @Bean
     public RomanNumeralConverter romanNumeralConverter() {
@@ -26,13 +29,13 @@ public class Application {
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
 
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
-
+        log.info("Let's inspect the beans provided by Spring Boot:");
         String[] beanNames = ctx.getBeanDefinitionNames();
         Arrays.sort(beanNames);
         for (String beanName : beanNames) {
-            System.out.println("Bean: " + beanName);
+            log.info(beanName);
         }
-    }
+		log.info("End initialization beans.");
+	}
 
 }
